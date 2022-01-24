550dbc7ef5666676b4c74288ea53da9d855c2afe
Svyatoslav <svyatoslave07@gmail.com>
2022-01-24 11:27:29 +0300

    -add component Tag & Paragraph(P)

---
+++ components/P/P.props.ts
@@ -0,0 +1,10 @@
+import { HTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
+
+export interface PProps
+  extends DetailedHTMLProps<
+    HTMLAttributes<HTMLParagraphElement>,
+    HTMLParagraphElement
+  > {
+  children: ReactNode;
+  size?: "s" | "m" | "l";
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
-import { Htag } from "../components";
+import { Htag, Button, P, Tag } from "../components";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Текст</Htag>
+      <Button appearance="primary" arrow="right">
+        Нажми
+      </Button>
+      <Button appearance="ghost" arrow="down">
+        Нажми
+      </Button>
+      <P size="s">Маленький</P>
+      <P size="m">Средний</P>
+      <P size="l">Большой</P>
+      <Tag size="s">Ghost</Tag>
+      <Tag size="m" color="red">
+        Red
+      </Tag>
+      <Tag size="s" color="green">
+        Green
+      </Tag>
+      <Tag color="primary">Primary</Tag>
     </>
   );
 }
